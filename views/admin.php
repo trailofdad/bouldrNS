21ca25537a67942250faf980ac46a72c6ed3add8
chapgood <christian.hapgood@redspace.com>
2017-10-30 15:58:02 -0300

    register plugin settings menu, clean up versions

--- views/admin.php
+++ views/admin.php
@@ -6,7 +6,7 @@
  * The User Interface to the end user.
  *
  * @package    gaston
- * @author     TODO
+ * @author     Christian Hapgood <christian.hapgood@gmail.com>
  * @license    GPL-2.0+
  * @link       TODO
  * @since      0.0.2
@@ -18,5 +18,8 @@
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
 	<!-- TODO: Provide markup for your options page here. -->
+	<h1>Hello world</h1>
+
+	<p>This is where the settings will go.</p>
 
 </div>
